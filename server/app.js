f132f8e26ee3e20db7c6b68fd7f4f77fa6356b5f
Y Paul Sussman <ypaulsussman@gmail.com>
2017-03-17 17:53:55 -0500

    completes backend calculation logic

--- server/app.js
+++ server/app.js
@@ -2,14 +2,33 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 var port = 5001;
 
+var toprint = "message returned";
+var result = {};
 
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('server/public/views/index.html'));
 });
+
+app.post('/calculate', function(req, res) {
+  if (req.body.type === "add") {
+    result.calc = parseFloat(req.body.x) + parseFloat(req.body.y);
+  } else if (req.body.type === "subtract") {
+    result.calc = parseFloat(req.body.x) - parseFloat(req.body.y);
+  } else if (req.body.type === "multiply") {
+    result.calc = parseFloat(req.body.x) * parseFloat(req.body.y);
+  } else if (req.body.type === "divide") {
+    result.calc = parseFloat(req.body.x) / parseFloat(req.body.y);
+  }
+  console.log("message received");
+  res.send(result);
+});
+
 
 app.listen(port,function() {
   console.log('listening on port', port);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,31 @@
 $(document).ready(function() {
 
-  $('button').on('click', function() {
-    var operand = $(this).attr('id');
-    operand = operand.slice(0, -6);
-    
+  var operand = "";
 
+  $('.operandButton').on('click', function() {
+    operand = $(this).data('operand');
+    return operand;
   });
 
+  $('#submitButton').on('click', function() {
+    if ($('#val01').val() && $('#val02').val() && operand) {
+      var calculation = {
+        x: $('#val01').val(),
+        y: $('#val02').val(),
+        type: operand,
+      };
+        $.ajax({
+        type: 'POST',
+        url: '/calculate',
+        data: calculation,
+        success: function (response) {
+          console.log(response);
+        }
+      });
+    } else {
+      console.log("finish yo calculation!");
+    }
+  });
 
 
 });
