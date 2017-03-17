d41778714bc92b13dbc59255fe3fd0930bbe69b7
Y Paul Sussman <ypaulsussman@gmail.com>
2017-03-17 18:08:38 -0500

    completes base-mode challenge

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 
   $('#submitButton').on('click', function() {
     if ($('#val01').val() && $('#val02').val() && operand) {
+      $('#unfinished').remove();
       var calculation = {
         x: $('#val01').val(),
         y: $('#val02').val(),
@@ -19,12 +20,18 @@ $(document).ready(function() {
         url: '/calculate',
         data: calculation,
         success: function (response) {
-          console.log(response);
+          $('.answer').append('<h1 id = "answer"> The answer is: ' + response.calc + '</h1>');
+          $('.clear').append('<button type="button" id="clearButton">Clear</button>');
         }
       });
     } else {
-      console.log("finish yo calculation!");
+      $('.answer').append('<h1 id = "unfinished">finish your calculation!</h1>');
     }
+  });
+
+  $('.clear').on('click', '#clearButton', function() {
+      $('.answer, .clear').empty();
+      $('#val01, #val02').val('');
   });
 
 
